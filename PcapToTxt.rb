63af90af63436ad54aa9b77fef6a070914f9de1c
Shen <sw29@cs.washington.edu>
2013-03-11 18:22:11 -0700

    Decided to use tcpdump instead of tshark
    Saves space and provides enough useful information

--- PcapToTxt.rb
+++ PcapToTxt.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/ruby
 
-# Takes in an directory path argument, converts all pcap
-# files into txt files with tshark. The txt files should
+# Takes in an directory path argument and a boolean, converts all pcap
+# files into txt files with tcpdump. The txt files should
 # be under whatever directory this script is called from.
+# Second argument should be either y or n. If it is y, files with the
+# same name will be overwritten, otherwise only new files will be
+# created. Old file will be safe
 
 path = ARGV[0]
+overwrite = ARGV[1] == "y"
 
 if(path[path.length-1] != "/") then path = path + "/" end
 
@@ -12,7 +16,9 @@ d = Dir.new(path)
 
 d.each do |file|
     if file.include?("pcap")
-	`tshark -Vr #{d.path + file} > #{file[0 .. (file.length - 5)] + "txt"}`
+	if (!File.exists?(file[0 .. (file.length - 5)] + "txt")) or overwrite
+	    `tcpdump -Ar #{d.path + file} > #{file[0 .. (file.length - 5)] + "txt"}`
+	end
     end
 end
 
